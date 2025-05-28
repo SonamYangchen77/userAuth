4265fba3db300aad496bcaf742d0d44c0019a1f8
SonamYangchen77 <07230064.sherubtse@rub.edu.bt>
2025-05-28 10:22:50 +0600

    userauth

--- Config/db.js
+++ Config/db.js
@@ -8,9 +8,7 @@ const db = pgp({
   database: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  ssl: false 
 });
 
 
